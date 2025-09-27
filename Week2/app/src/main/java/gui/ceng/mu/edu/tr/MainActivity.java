3ed1d6d1b99452a29f5eb5651f5b3135d381921e
ahsenpehlivan <ahsenpehlivan007@gmail.com>
2025-09-27 20:15:40 +0300

    Week 2

---
+++ Week2/app/src/main/java/gui/ceng/mu/edu/tr/MainActivity.java
@@ -0,0 +1,33 @@
+package gui.ceng.mu.edu.tr;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnArrayAdapter;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnArrayAdapter = (Button) findViewById(R.id.btnArrayAdapter);
+        btnArrayAdapter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ArrayAdapterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
